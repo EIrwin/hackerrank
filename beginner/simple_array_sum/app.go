301e1eb090f591e2fe9a28dfe4b532247fba088b
= <Eric.Irwin1124@gmail.com>
2016-06-21 19:51:51 -0700

    initial commit

---
+++ beginner/simple_array_sum/app.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+
+	//grab number of arguments
+	var num int
+	fmt.Scanln(&num)
+	
+	//sum up input string
+	sum := 0
+	for i:= 0; i < num;i++ {
+		var val int
+		fmt.Scan(&val)
+		sum += val
+	}
+
+	fmt.Println(sum)
+}
